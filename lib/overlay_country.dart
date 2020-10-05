88f44f8afaff23870854ee81ee0558687cc60f66
fservingtech <trong.nguyen@smartdev.vn>
2020-10-05 18:08:11 +0700

    Overlay

--- lib/overlay_country.dart
+++ lib/overlay_country.dart
@@ -24,61 +24,63 @@ class OverlayCountry {
                 link: layerLink,
                 showWhenUnlinked: false,
                 offset: Offset(offset.dx, size.height + 6),
-                child: Container(
-                  width: size.width,
-                  height: height ?? 300,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: border ?? Colors.yellow, width: 1),
-                    borderRadius: BorderRadius.circular(borderRadius ?? 4)
-                  ),
-                  child: ListView.builder(
-                    padding: EdgeInsets.all(8),
-                    itemCount: _listCountry.length,
-                    physics: AlwaysScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      Country _country = _listCountry[index];
-                      return InkWell(
-                          onTap: () => onTapItem(_country),
-                          child: Container(
-                            padding: EdgeInsets.only(bottom: index == _listCountry.length - 1 ? 0 : 8),
-                            child: Row(
-                              children: [
-                                Container(
-                                  child: Image.asset(
-                                    _country.asset,
-                                    package: "flutter_country_picker",
-                                    height: 24.0,
-                                    fit: BoxFit.fitWidth,
-                                  ),
-                                ),
-                                Flexible(
-                                  child: Container(
-                                    margin: EdgeInsets.only(left: 8),
-                                    child: Row(
-                                      children: [
-                                        Flexible(
-                                          child: Container(
-                                              child: Text(
-                                                _country.name.toUpperCase(),
-                                                overflow: TextOverflow.ellipsis,
-                                                style: countryTextStyle,
-                                                maxLines: 1,)),
-                                        ),
-                                        Container(
-                                          margin: EdgeInsets.only(left: 8),
-                                          child: Text(
-                                            "+" + _country.dialingCode,
-                                            style: dialingCodeTextStyle,
-                                          ),
-                                        )
-                                      ],
+                child: Material(
+                  child: Container(
+                    width: size.width,
+                    height: height ?? 300,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: border ?? Colors.yellow, width: 1),
+                      borderRadius: BorderRadius.circular(borderRadius ?? 4)
+                    ),
+                    child: ListView.builder(
+                      padding: EdgeInsets.all(8),
+                      itemCount: _listCountry.length,
+                      physics: AlwaysScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        Country _country = _listCountry[index];
+                        return InkWell(
+                            onTap: () => onTapItem(_country),
+                            child: Container(
+                              padding: EdgeInsets.only(bottom: index == _listCountry.length - 1 ? 0 : 8),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    child: Image.asset(
+                                      _country.asset,
+                                      package: "flutter_country_picker",
+                                      height: 24.0,
+                                      fit: BoxFit.fitWidth,
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ));
-                    },
+                                  Flexible(
+                                    child: Container(
+                                      margin: EdgeInsets.only(left: 8),
+                                      child: Row(
+                                        children: [
+                                          Flexible(
+                                            child: Container(
+                                                child: Text(
+                                                  _country.name.toUpperCase(),
+                                                  overflow: TextOverflow.ellipsis,
+                                                  style: countryTextStyle,
+                                                  maxLines: 1,)),
+                                          ),
+                                          Container(
+                                            margin: EdgeInsets.only(left: 8),
+                                            child: Text(
+                                              "+" + _country.dialingCode,
+                                              style: dialingCodeTextStyle,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ));
+                      },
+                    ),
                   ),
                 ),
               ),
